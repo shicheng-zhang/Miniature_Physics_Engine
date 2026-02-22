5b7f8ddbbc576c275d703ffca44669cacf9c5b23
shicheng-zhang <shichenghome@gmail.com>
2026-02-22 13:59:40 -0700

    Added Camera Movement and Motion FOV to Stage 2

--- stage2/interface/sphere_object/meshing/sphere_meshing.h
+++ stage2/interface/sphere_object/meshing/sphere_meshing.h
@@ -1,10 +1,8 @@
 #ifndef sphere_mesh_h
 #define sphere_mesh_h
-
 #include <epoxy/gl.h>
 #include "../../master_header_2.h"
 #include "../../../stage1/master_header.h"
-
 //GLuint = GPU buffer ID
 typedef struct {
     GLuint vao, vbo, ebo;
@@ -13,7 +11,6 @@ typedef struct {
     //ebo = element buffer object, index store and category
     int index; //Number of points to draw
 } mesh;
-
 //Function Pre-declaration
 void init_sm_system (mesh *mesh_s, int sections, int stack);
 void render_sphere_object (mesh * mesh_s, rigidbody *rb, math3 projections, math3 viewpoint);
