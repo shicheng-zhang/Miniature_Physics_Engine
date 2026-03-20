4380a1c3ae1df41335ca2c60edd99e8d78353f7d
shicheng-zhang <shichenghome@gmail.com>
2026-03-19 18:37:35 -0600

    19/03/26: Added math4 type definition, expanded upon rendering synaptic work with math4 viewports

--- stage1/master_header.h
+++ stage1/master_header.h
@@ -2,6 +2,7 @@
 #define master_h
 //master header file for calling all library files
 #include "math/math3D.h" //Math
+#include "math/math4_special.h" //Math
 #include "math_phys_buffer/buffer.h" //Buffer
 #include "physics/collision_mechanics/collision_mechanics.h" //Collision Mechanics
 #include "physics/forces/defines_majority_forces/define_forces.h" //Ordinary Physics (Forces and Energy)
--- stage2/master_header_2.h
+++ stage2/master_header_2.h
@@ -1,7 +1,26 @@
 #ifndef master_header_2_h
 #define master_header_2_h
-#include "continued_loop/timer.h"
-#include "interface/camera_view/camera_pov.h"
-#include "interface/gui/gui.c"
-#include "interface/simulate/simulation.c"
-#include "interface/render/render.c"
+
+#include "../stage1/master_header.h"
+#include "camera/camera.h"
+#include "input_controller/input_control.h"
+#include "interface/sphere_object/meshing/sphere_meshing.h"
+
+// Global scene, interface/simulate/simulation.c
+extern rigidbody obj_per_scene [10];
+extern int object_count;
+
+// Global camera and input, root_gtk.c
+extern camera main_camera_fov;
+extern input_status main_inputs;
+
+// Rendering, interface/render/render.c
+void render_scene_current (int width, int height);
+
+// Physics interface, interface/simulate/simulation.c
+gboolean physics_step_increment (gpointer user_data_stored);
+
+// GUI, interface/gui/gui.c
+void activation (GtkApplication *application, gpointer user_data_main);
+
+#endif // master_header_2_h
